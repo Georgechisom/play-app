97cddf647b16757c1dcd74c6e3f67566a8b13513
george chisom <georgechipaul@gmail.com>
2025-07-22 01:06:42 +0100

    app updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import abi from "./abi.json";
-import "../src/myCss.css";
+import "./Css/special.css";
 import { ethers } from "ethers";
 
 const contractAddress = "0xee053B950ea5b88Db90288D1E92f7a293dDe3628"; //this is the contract address after i have deployed the contract
@@ -82,7 +82,7 @@ function App() {
       setViewOut("Message Fetched Below!")
     } catch (error) {
       console.error("Error fetching message:", error);
-      setViewOut("Error fetching message");
+      setViewOut("Reponds Below");
       setGetOutError(
         "Failed to fetch message: " +
           (error.message || "Check console for details")
@@ -95,26 +95,26 @@ function App() {
   }, [handleGet]);
 
   return (
-    <div className="firstdiv" style={{ padding: "2rem", display: "flex", justifyContent: "center", alignItems: "center", }}>
-      <div className="seconddiv" style={{ position: "absolute", top: "25%", border: "2px solid blue", width: "70%", padding: "3% 3%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+    <div className="firstdiv" >
+      <div className="seconddiv" >
         <h1 style={{ textAlign: "center", fontSize: "25px" }}>Smart Contract</h1>
         <input
           type="text"
           placeholder="Set message"
           value={text}
-          style={{ margin: "1.5rem 1.5rem", width: "70%"}}
+          className="uniInput"
           onChange={(e) => setText(e.target.value)}
         />
-        <button style={{ margin: "1rem 1rem", padding: "3% 3%", width: "70%", border: "none", backgroundColor: "blue", color: "white", borderRadius: "5%" }}  onClick={handleSet}>{viewIn}</button>
+        <button className="firstbtn" onClick={handleSet} >{viewIn}</button>
         <div style={{ overflow: "hidden"}}>
-          {message && <p style={{ overflow: "hidden"}}>Message: {message}</p>}
-          {getInError && <p style={{ color: "red" }}>Error: {getInError}</p>}
+          {message && <p className="good">Message: {message}</p>}
+          {getInError && <p className="errors">Error: {getInError}</p>}
         </div>
 
-        <button style={{ margin: "1rem 1rem", padding: "3% 3%", width: "70%", border: "none", backgroundColor: "blue", color: "white", borderRadius: "5%" }} onClick={handleGet}>{viewOut}</button>
-        <div>
-          {outMessage && <p style={{ overflow: "hidden"}}>Message: {outMessage}</p>}
-          {getOutError && <p style={{ color: "red" }}>Error: {getOutError}</p>}
+        <button className="secbtn" onClick={handleGet}>{viewOut}</button>
+        <div style={{ overflow: "hidden"}}>
+          {outMessage && <p className="good">Message: {outMessage}</p>}
+          {getOutError && <p className="erroes">Error: {getOutError}</p>}
         </div>
         </div>
     </div>
